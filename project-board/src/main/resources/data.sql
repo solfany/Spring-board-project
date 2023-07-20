c37eceaf7dcfe0b6ea853f39753d77ea366c46d8
solfany <solfany1999@gmail.com>
2023-07-20 12:32:59 +0900

    #12 - jpa test 작성
    기본 crud + 연관 관계 매핑 과 casscad

---
+++ project-board/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into article (title, content, hashtag, created_by, modified_by, created_at, modified_at) values
+    ('Quisque ut erat.', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestib', '#hashtag', 'created_user', 'modified_user', '2023-07-20', '2023-07-20');
