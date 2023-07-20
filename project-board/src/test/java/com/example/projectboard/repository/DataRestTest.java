ef04a3cfcb01c488394edfb7ad9d1c9bbcb492c5
solfany <solfany1999@gmail.com>
2023-07-20 18:01:00 +0900

    #3 - data rest 테스트
    꼭 필요한 테스트는 아니지만 공부 겸 
    data rest 테스트는 인테그레이션 테스트로 접근 할 수 밖에  없다 
    
    db 접근이 일어나므로 안전을 위해 `@Transactional` 적용 
    테스트 안에서는 롤백 정책으로 동작한다.

---
+++ project-board/src/test/java/com/example/projectboard/repository/DataRestTest.java
@@ -0,0 +1,37 @@
+package com.example.projectboard.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Data_REST_Api 테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc){
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[api] 게시글 조회")
+    @Test
+    void givenNotting_whenRequestingArticles_thenReturnArticlesJsonResponse() throws Exception {
+        // Given
+
+        // Whem & Then
+        mvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+}
