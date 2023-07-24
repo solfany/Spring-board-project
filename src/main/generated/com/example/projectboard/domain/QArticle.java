b83165fe04762b098cc6490657bf6bf0107b7f4c
solfany <solfany1999@gmail.com>
2023-07-24 15:56:12 +0900

    #main - 폴더 정리

---
+++ src/main/generated/com/example/projectboard/domain/QArticle.java
@@ -0,0 +1,54 @@
+package com.example.projectboard.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QArticle is a Querydsl query type for Article
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QArticle extends EntityPathBase<Article> {
+
+    private static final long serialVersionUID = 1230569948L;
+
+    public static final QArticle article = new QArticle("article");
+
+    public final SetPath<ArticleComment, QArticleComment> articleComments = this.<ArticleComment, QArticleComment>createSet("articleComments", ArticleComment.class, QArticleComment.class, PathInits.DIRECT2);
+
+    public final StringPath content = createString("content");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final StringPath createdBy = createString("createdBy");
+
+    public final StringPath hashtag = createString("hashtag");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = createDateTime("modifiedAt", java.time.LocalDateTime.class);
+
+    public final StringPath modifiedBy = createString("modifiedBy");
+
+    public final StringPath title = createString("title");
+
+    public QArticle(String variable) {
+        super(Article.class, forVariable(variable));
+    }
+
+    public QArticle(Path<? extends Article> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QArticle(PathMetadata metadata) {
+        super(Article.class, metadata);
+    }
+
+}
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO article (title, content, hashtag, created_by, modified_by, created_at, modified_at)
+VALUES ('Quisque ut erat.', 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestib', '#hashtag', 'created_user', 'modified_user', '2023-07-20', '2023-07-20');
