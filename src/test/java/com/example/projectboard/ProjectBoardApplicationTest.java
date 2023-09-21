56a495b377f77b158cb07f1b36cdae5d4b7f202b
solfany <solfany1999@gmail.com>
2023-09-21 20:29:11 +0900

    #테스트 코드 오류 해결
    프로젝트 실행 이름과 테스트 코드 실행 이름이 달라 빌드가 안되었고, 인텔리제이 프로퍼티 버전이 gradle로 설정되어있어서 IntelliJ IDEA로 변경해주었다.

---
+++ src/test/java/com/example/projectboard/ProjectBoardApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProjectBoardApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
