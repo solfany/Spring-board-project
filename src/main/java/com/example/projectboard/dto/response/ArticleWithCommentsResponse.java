b3a97e0b62153f0d81efbd39c312332ee78d143d
solfany <solfany1999@gmail.com>
2023-09-26 15:57:26 +0900

    #35 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드 작성

--- src/main/java/com/example/projectboard/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/com/example/projectboard/dto/response/ArticleWithCommentsResponse.java
@@ -1,46 +1,73 @@
 package com.example.projectboard.dto.response;
 
+import com.example.projectboard.dto.ArticleCommentDto;
 import com.example.projectboard.dto.ArticleWithCommentsDto;
+import com.example.projectboard.dto.HashtagDto;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public record ArticleWithCommentsResponse(
-        Long id,
-        String title,
-        String content,
-        String hashtag,
-        LocalDateTime createdAt,
-        String email,
-        String nickname,
-        Set<ArticleCommentResponse> articleCommentsResponse
-) implements Serializable {
+  Long id,
+  String title,
+  String content,
+  Set<String> hashtags,
+  LocalDateTime createdAt,
+  String email,
+  String nickname,
+  String userId,
+  Set<ArticleCommentResponse> articleCommentsResponse
+) {
 
-    public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
-        return new ArticleWithCommentsResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
+  public static ArticleWithCommentsResponse of(Long id, String title, String content, Set<String> hashtags, LocalDateTime createdAt, String email, String nickname, String userId, Set<ArticleCommentResponse> articleCommentResponses) {
+    return new ArticleWithCommentsResponse(id, title, content, hashtags, createdAt, email, nickname, userId, articleCommentResponses);
+  }
+
+  public static ArticleWithCommentsResponse from(ArticleWithCommentsDto dto) {
+    String nickname = dto.userAccountDto().nickname();
+    if (nickname == null || nickname.isBlank()) {
+      nickname = dto.userAccountDto().userId();
     }
 
-    public static ArticleWithCommentsResponse from(ArticleWithCommentsDto dto) {
-        String nickname = dto.userAccountDto().nickname();
-        if (nickname == null || nickname.isBlank()) {
-            nickname = dto.userAccountDto().userId();
-        }
+    return new ArticleWithCommentsResponse(
+      dto.id(),
+      dto.title(),
+      dto.content(),
+      dto.hashtagDtos().stream()
+        .map(HashtagDto::hashtagName)
+        .collect(Collectors.toUnmodifiableSet())
+      ,
+      dto.createdAt(),
+      dto.userAccountDto().email(),
+      nickname,
+      dto.userAccountDto().userId(),
+      organizeChildComments(dto.articleCommentDtos())
+    );
+  }
 
-        return new ArticleWithCommentsResponse(
-                dto.id(),
-                dto.title(),
-                dto.content(),
-                dto.hashtag(),
-                dto.createdAt(),
-                dto.userAccountDto().email(),
-                nickname,
-                dto.articleCommentDtos().stream()
-                        .map(ArticleCommentResponse::from)
-                        .collect(Collectors.toCollection(LinkedHashSet::new))
-        );
-    }
 
+  private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
+    Map<Long, ArticleCommentResponse> map = dtos.stream()
+      .map(ArticleCommentResponse::from)
+      .collect(Collectors.toMap(ArticleCommentResponse::id, Function.identity()));
+
+    map.values().stream()
+      .filter(ArticleCommentResponse::hasParentComment)
+      .forEach(comment -> {
+        ArticleCommentResponse parentComment = map.get(comment.parentCommentId());
+        parentComment.childComments().add(comment);
+      });
+
+    return map.values().stream()
+      .filter(comment -> !comment.hasParentComment())
+      .collect(Collectors.toCollection(() ->
+        new TreeSet<>(Comparator
+          .comparing(ArticleCommentResponse::createdAt)
+          .reversed()
+          .thenComparingLong(ArticleCommentResponse::id)
+        )
+      ));
+  }
 }
--- src/main/java/com/example/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/example/projectboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,7 @@ package com.example.projectboard.repository;
 
 import com.example.projectboard.domain.ArticleComment;
 import com.example.projectboard.domain.QArticleComment;
+import com.example.projectboard.domain.projection.ArticleCommentProjection;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,21 +13,22 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = ArticleCommentProjection.class)
 public interface ArticleCommentRepository extends
-        JpaRepository<ArticleComment, Long>,
-        QuerydslPredicateExecutor<ArticleComment>,
-        QuerydslBinderCustomizer<QArticleComment> {
+  JpaRepository<ArticleComment, Long>,
+  QuerydslPredicateExecutor<ArticleComment>,
+  QuerydslBinderCustomizer<QArticleComment> {
 
-    List<ArticleComment> findByArticle_Id(Long articleId);
+  List<ArticleComment> findByArticle_Id(Long articleId);
+  void deleteByIdAndUserAccount_UserId(Long articleCommentId, String userId);
 
-    @Override
-    default void customize(QuerydslBindings bindings, QArticleComment root) {
-        bindings.excludeUnlistedProperties(true);
-        bindings.including(root.content, root.createdAt, root.createdBy);
-        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
-        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
-        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
-    }
+  @Override
+  default void customize(QuerydslBindings bindings, QArticleComment root) {
+    bindings.excludeUnlistedProperties(true);
+    bindings.including(root.content, root.createdAt, root.createdBy);
+    bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+    bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+  }
 
 }
