756ad74730ab17c67b8bd39b7bde34e38177066f
solfany <solfany1999@gmail.com>
2023-09-26 16:16:09 +0900

    시큐리티, 및 카카오 연동  코드 추가

---
+++ bin/generated-sources/annotations/com/example/projectboard/domain/QHashtag.java
@@ -0,0 +1,56 @@
+package com.example.projectboard.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QHashtag is a Querydsl query type for Hashtag
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QHashtag extends EntityPathBase<Hashtag> {
+
+    private static final long serialVersionUID = -1634471758L;
+
+    public static final QHashtag hashtag = new QHashtag("hashtag");
+
+    public final QAuditingFields _super = new QAuditingFields(this);
+
+    public final SetPath<Article, QArticle> articles = this.<Article, QArticle>createSet("articles", Article.class, QArticle.class, PathInits.DIRECT2);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    public final StringPath hashtagName = createString("hashtagName");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    //inherited
+    public final StringPath modifiedBy = _super.modifiedBy;
+
+    public QHashtag(String variable) {
+        super(Hashtag.class, forVariable(variable));
+    }
+
+    public QHashtag(Path<? extends Hashtag> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QHashtag(PathMetadata metadata) {
+        super(Hashtag.class, metadata);
+    }
+
+}
+
---
+++ src/main/java/com/example/projectboard/dto/request/ArticleRequest.java
@@ -0,0 +1,31 @@
+package com.example.projectboard.dto.request;
+
+import com.example.projectboard.dto.ArticleDto;
+import com.example.projectboard.dto.HashtagDto;
+import com.example.projectboard.dto.UserAccountDto;
+
+import java.util.Set;
+
+public record ArticleRequest(
+  String title,
+  String content
+) {
+
+  public static ArticleRequest of(String title, String content) {
+    return new ArticleRequest(title, content);
+  }
+
+  public ArticleDto toDto(UserAccountDto userAccountDto) {
+    return toDto(userAccountDto, null);
+  }
+
+  public ArticleDto toDto(UserAccountDto userAccountDto, Set<HashtagDto> hashtagDtos) {
+    return ArticleDto.of(
+      userAccountDto,
+      title,
+      content,
+      hashtagDtos
+    );
+  }
+
+}
